e07a8187c19f9c624aff15ef6d7e6c4830fd67dd
Joao Bateloche <bateloche@outlook.com>
2018-02-25 02:55:07 -0300

    Correção da frase final

--- app/evolucao.py
+++ app/evolucao.py
@@ -13,7 +13,7 @@ def caracteres_diferentes(texto, texto_alterado):
 
 if __name__ == "__main__":
     gerador = GeradorStrings()
-    fim = "METHINKS IT IS A WEASEL"
+    fim = "METHINKS IT IS LIKE A WEASEL"
     inicio = gerador.nova(len(fim))
 
     atual = inicio
@@ -21,7 +21,7 @@ if __name__ == "__main__":
     i = 1
     while atual != fim:
         reproducao = Reproducao(atual, mutacao)
-        nova_geracao = reproducao.criar_geracao(1000)
+        nova_geracao = reproducao.criar_geracao(500)
         analise = []
         for filho in nova_geracao:
             heappush(analise, (caracteres_diferentes(filho, fim), filho))
